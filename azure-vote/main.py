d594f4547dd160001fcb10d870d7cb7fe69da2af
yalhojaily <yalhojaily@gmail.com>
2022-11-09 04:35:33 +0300

    add tracer

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -118,12 +118,12 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
-            logger.info('Cats', extra=properties)
+            logger.warning('Cats', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
-            logger.info('Dogs', extra=properties)
+            logger.warning('Dogs', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
