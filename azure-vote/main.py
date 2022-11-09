9d96331ec2a8d4eba5941f0b11bf1f1dfb96a978
yalhojaily <yalhojaily@gmail.com>
2022-11-09 04:11:05 +0300

    clean!

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -17,7 +17,7 @@ from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
 from opencensus.stats import view as view_module
 from opencensus.tags import tag_map as tag_map_module
-#from opencensus.trace import config_integration
+from opencensus.trace import config_integration
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
