254fbc2807b95f3b513fc78c586aec37ce658923
yalhojaily <yalhojaily@gmail.com>
2022-11-09 04:56:13 +0300

    add tracer

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -99,11 +99,11 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tracer.span(name="Cats Votes")
+        tracer.span(name="CatsVote")
 
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tracer.span(name="Dogs Votes")
+        tracer.span(name="DogsVote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
